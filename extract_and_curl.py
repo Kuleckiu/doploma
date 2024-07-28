fac2ceb8737dd529b082a286bbc9d937e530eee4
Kuleckiu <stas.kuleckiy@mail.ru>
2024-07-28 15:26:58 +0300

    enougth

--- extract_and_curl.py
+++ extract_and_curl.py
@@ -18,25 +18,10 @@ def check_service(ip_address):
     response = subprocess.run(['curl', '-s', '-o', '/dev/null', '-w', '%{http_code}', f'http://{ip_address}:5601/login'], capture_output=True, text=True)
     return response.stdout.strip()
 
-# Usage example
-# inventory_file = 'ansible/inventories/inventory'
-# ip_address = extract_ip_from_inventory(inventory_file)
-# print(f"IP-the address from the file inventory: {ip_address}")
-# # time.sleep(160)
-# http_code = check_service(ip_address)
-# print(f"HTTP response code: {http_code}")
-
-# Return the completion code for use in Jenkins
-# if http_code == "200":
-#     exit(0)
-# else:
-#     time.sleep(10)
-#     http_code = check_service(ip_address)
-#     print(f"HTTP response code: {http_code}")
 def main():
     inventory_file = 'ansible/inventories/inventory'
     ip_address = extract_ip_from_inventory(inventory_file)
-    print(f"IP-адрес из инвентори файла: {ip_address}")
+    print(f"IP-address from the file inventory: {ip_address}")
 
     while True:
         http_code = check_service(ip_address)
@@ -44,7 +29,7 @@ def main():
         if http_code == "200":
             exit(0)
         else:
-            print("Сервис недоступен. Повторная проверка через 10 секунд...")
+            print("The service is unavailable. Re-check in 10 seconds...")
             time.sleep(10)
 
 if __name__ == "__main__":
